d8ab2d59cc52fd34757eb810a7f90a4e882c3802
thelastnig <thelastnig@gmail.com>
2020-04-20 14:36:35 +0900

    Updates

---
+++ precache-manifest.4ddefe9789c5d253ebe83cc1abf5bd9c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768862ac1cc115e26dbe9a154b30704a",
+    "url": "/data-frontend/index.html"
+  },
+  {
+    "revision": "d716c0fc57ccbd512dc9",
+    "url": "/data-frontend/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "ba40ad9df0fb141f60e4",
+    "url": "/data-frontend/static/css/main.49ccb725.chunk.css"
+  },
+  {
+    "revision": "d716c0fc57ccbd512dc9",
+    "url": "/data-frontend/static/js/2.825376e2.chunk.js"
+  },
+  {
+    "revision": "93d6ce2f533dbfa12b2db314413182db",
+    "url": "/data-frontend/static/js/2.825376e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba40ad9df0fb141f60e4",
+    "url": "/data-frontend/static/js/main.ce41eec9.chunk.js"
+  },
+  {
+    "revision": "b86249da130e2afded6b",
+    "url": "/data-frontend/static/js/runtime-main.10b9b045.js"
+  },
+  {
+    "revision": "e2f4ccf68b087fe82c5a9b0c7fa5f4da",
+    "url": "/data-frontend/static/media/cast.e2f4ccf6.png"
+  },
+  {
+    "revision": "098fd59717524c36855a0b1ddf9c63d7",
+    "url": "/data-frontend/static/media/chapter.098fd597.png"
+  },
+  {
+    "revision": "68b535a65fa04d13d14b439f2b467ae2",
+    "url": "/data-frontend/static/media/network.68b535a6.png"
+  }
+]);
